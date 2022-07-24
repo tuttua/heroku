57bd3fe07fa6afcd614536e8d116ad8c8e45fc21
tuttua <109905885+tuttua@users.noreply.github.com>
2022-07-24 18:33:10 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+email = \"21f1001562@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
