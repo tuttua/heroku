868a4594c852641151b32bcbb37b781f57e7c185
tuttua <109905885+tuttua@users.noreply.github.com>
2022-07-24 21:46:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 mkdir -p ~/.streamlit/
 
+echo :\
+[general]\n\
+email = \"21f1001562@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
